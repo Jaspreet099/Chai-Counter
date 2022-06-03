01cef435055de026c0241ec0423bf738aa0b923e
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-06-03 22:06:56 +0530

    LoginSystem added and nav bar created

---
+++ partials/_nav.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Chai Counter</title>
+</head>
+<body>
+<header class="text-gray-600 body-font">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <img src="../images/logo.png" alt="logo">
+      <!-- <span class="ml-3 text-xl">Tailblocks</span> -->
+    </a>
+    <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center mt-6">
+      <a class="btn1 mr-5">Counter</a>
+      <a class="btn2 mr-5 ml-11">Login/SignUp</a>
+    </nav>
+  </div>
+</header>
+</body>
+</html>
