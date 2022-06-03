05c8f4429ccd28a75fa14cf5868619e53f00df71
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-06-03 23:40:39 +0530

    Login page css

--- LoginSystem/login.php
+++ LoginSystem/login.php
@@ -5,36 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../partials/style.css">
+    <link rel="stylesheet" href="../LoginSystem/login.css">
     <title>Chai Counter ~ Login</title>
 </head>
 <body>
     <?php include '../partials/_nav.php'
     
     ?>
-    <section class="text-gray-600 body-font relative">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col text-center w-full mb-12">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+    <section class="">
+  <div class="login container none mx-auto w-3/12">
+    <div class="flex flex-col text-center">
+      <h1 class="head center">Login</h1>
     </div>
     <div class="lg:w-1/2 md:w-2/3 mx-auto">
       <div class="flex flex-wrap -m-2">
-        <div class="p-2 w-full">
+        <div class=" w-full mt-11">
           <div class="relative">
-            <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-            <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            
+            <input type="text" id="name" name="name" class="Uname">
           </div>
         </div>
-        <div class="p-2 w-full">
+        <div class="w-full mt-8">
           <div class="relative">
-            <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-            <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-          </div>
-        </div>
-        <div class="p-2 w-full">
-          <div class="relative">
-            <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-            <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+            
+            <input type="password" name="pass" class="pass">
           </div>
         </div>
         <div class="p-2 w-full">
