eb290afa95eff9a39da6590167c80f34fbe6f49d
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-06-05 20:19:22 +0530

    Login & Signup created but PHP database not working

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -2,15 +2,14 @@
 
 $hostname = "localhost";
    $userid = "root";
-   $pass = "";
-   $dbname = "chai-counter";
-   
+   $pass = "sneha";
+   $dbname = "phpwebsite";
    $conn = new mysqli($hostname,$userid,$pass,$dbname);
    
    if(!$conn){
        echo "error in getting connection";
    }
    else{
-         echo "d";
+         echo "";
    }
 ?>
